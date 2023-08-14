dc0bf555096b4e051b375a5da90d9bc6e48ca5af
prateeksahu147 <prateeksahu147@gmail.com>
2023-08-15 02:05:35 +0530

    Production Ready Code

--- app/neural_roots/settings.py
+++ app/neural_roots/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 #DEBUG = True
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "neuralroots.in", "www.neuralroots.in"]
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
 #ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
